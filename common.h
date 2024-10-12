93d43b88a7f83036003bff9654dea307afaecd63
rtmath <ceo@ligma.org>
2024-10-12 07:46:35 -0700

    remove absolutely dreadful attempt at preventing a macro from being defined more than once

--- common.h
+++ common.h
@@ -1,8 +1,6 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#ifndef ROOM_FOR_NULL_BYTE
-  #define ROOM_FOR_NULL_BYTE 1
-#endif /* ROOM_FOR_NULL_BYTE */
+#define ROOM_FOR_NULL_BYTE 1
 
-#endif /* COMMON_H */
+#endif
