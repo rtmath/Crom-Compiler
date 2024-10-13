06420526258ab3f68727a8425664f516d7a6eb4f
rtmath <ceo@ligma.org>
2024-10-12 21:27:15 -0700

    load test.txt and call Compile() from main

--- main.c
+++ main.c
@@ -1,3 +1,14 @@
+#include <stddef.h> // for NULL
+
+#include "compiler.h"
+#include "io.h"
+
 int main(void) {
+  const char *filename = "test.txt";
+  char *contents = NULL;
+  ReadFile(filename, &contents);
+
+  Compile(contents);
+
   return 0;
 }
