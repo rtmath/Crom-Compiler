b3d500eee6f9486a95d11078a7f22af70b51cbde
rtmath <ceo@ligma.org>
2024-10-13 22:18:12 -0700

    update TokenTypeTranslation table

--- token_type.c
+++ token_type.c
@@ -2,10 +2,21 @@
 
 static const char* const _TokenTypeTranslation[] =
 {
+  [I8]  = "I8", [I16] = "I16", [I32] = "I32", [I64] = "I64",
+  [U8]  = "U8", [U16] = "U16", [U32] = "U32", [U64] = "U64",
+  [F32] = "F32", [F64] = "F64",
+  [CHAR] = "CHAR", [STRING] = "STRING",
+  [BOOL] = "BOOL",
+  [VOID] = "VOID",
+  [ENUM] = "ENUM",
+  [STRUCT] = "STRUCT",
+
   [IDENTIFIER] = "IDENTIFIER",
 
   [INT_CONSTANT] = "INT",
   [FLOAT_CONSTANT] = "FLOAT",
+
+  [STRING_LITERAL] = "STRING_LITERAL",
 
   [LCURLY] = "LCURLY", [RCURLY] = "RCURLY",
   [SEMICOLON] = "SEMICOLON",
