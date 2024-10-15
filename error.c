bb0b94f3f3880fcf9aa91e355c06e99a83a43add
rtmath <ceo@ligma.org>
2024-10-14 20:47:40 -0700

    add ErrorAndContinue function. rename ErrorExit to ErrorAndExit

--- error.c
+++ error.c
@@ -4,15 +4,31 @@
 
 #include "error.h"
 
-void ErrorExit(const char* src_filename, int line_number,
-               const char *fmt_string, ...)
-{
+static void PrintVariadic(const char *fmt_string, va_list args) {
+  vprintf(fmt_string, args);
+}
+
+void ErrorAndContinue(const char *src_filename, int line_number,
+                      const char *fmt_string, ...) {
+  // va_list necessary for passing '...' to another function
   va_list args;
   va_start(args, fmt_string);
 
   printf("[%s:%i] ", src_filename, line_number);
-  vprintf(fmt_string, args);
+  PrintVariadic(fmt_string, args);
   printf("\n");
+
+  va_end(args);
+}
+
+void ErrorAndExit(const char* src_filename, int line_number,
+                  const char *fmt_string, ...)
+{
+  // va_list necessary for passing '...' to another function
+  va_list args;
+  va_start(args, fmt_string);
+
+  ErrorAndContinue(src_filename, line_number, fmt_string, args);
 
   va_end(args);
 
--- error.h
+++ error.h
@@ -1,9 +1,13 @@
 #ifndef ERROR_H
 #define ERROR_H
 
-#define ERROR_EXIT(fmt, ...) ErrorExit(__FILE__, __LINE__, fmt, __VA_ARGS__)
+#define ERROR_AND_CONTINUE(fmt, ...) ErrorAndContinue(__FILE__, __LINE__, fmt, __VA_ARGS__)
+#define ERROR_AND_EXIT(fmt, ...) ErrorAndExit(__FILE__, __LINE__, fmt, __VA_ARGS__)
 
-void ErrorExit(const char* src_filename, int line_number,
-               const char *fmt_string, ...);
+void ErrorAndContinue(const char *src_filename, int line_number,
+                      const char *fmt_string, ...);
+
+void ErrorAndExit(const char *src_filename, int line_number,
+                  const char *fmt_string, ...);
 
 #endif
