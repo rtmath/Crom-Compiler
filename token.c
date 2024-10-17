271d4cd7b2dff4287f9d9ef5e354a5454fb4324f
rtmath <ceo@ligma.org>
2024-10-16 17:04:37 -0700

    change PrintToken format

--- token.c
+++ token.c
@@ -3,8 +3,7 @@
 #include "token.h"
 
 void PrintToken(Token t) {
-  printf("[Line %3d|%10s] %.*s\n",
-      t.on_line,
+  printf("%s(%.*s)\n",
       TokenTypeTranslation(t.type),
       t.length,
       t.position_in_source);
