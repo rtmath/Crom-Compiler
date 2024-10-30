e767505c290ed23ccf2e63f5640173ec6c1695d3
rtmath <ceo@ligma.org>
2024-10-30 12:53:29 -0700

    update Type Checker to handle Declarations and Assignments, modify Identifier-handling behvaior (fixing a bug with identifier assignments)

--- type_checker.c
+++ type_checker.c
@@ -8,6 +8,8 @@
 #include "parser_annotation.h"
 #include "type_checker.h"
 
+static SymbolTable *SYMBOL_TABLE;
+
 /* === HELPERS === */
 static int BitWidth(AST_Node *node) {
   return node->annotation.bit_width;
@@ -43,31 +45,31 @@ void CheckTypeDisagreement(AST_Node *a, AST_Node *b) {
                  ActualTypeTranslation(b->annotation.actual_type));
 }
 
-long long StringToLL(const char *s) {
+long long TokenToLL(Token t) {
   errno = 0;
-  long long value = strtoll(s, NULL, 10);
+  long long value = strtoll(t.position_in_source, NULL, 10);
   if (errno != 0) {
-    ERROR_AND_EXIT("StringToLL() error");
+    ERROR_AND_EXIT("TokenToLL() error");
   }
 
   return value;
 }
 
-unsigned long long StringToULL(const char *s) {
+unsigned long long TokenToULL(Token t) {
   errno = 0;
-  unsigned long long value = strtoull(s, NULL, 10);
+  unsigned long long value = strtoull(t.position_in_source, NULL, 10);
   if (errno != 0) {
-    ERROR_AND_EXIT("StringToLL() error");
+    ERROR_AND_EXIT("TokenToULL() error");
   }
 
   return value;
 }
 
-double StringToDouble(const char *s) {
+double TokenToDouble(Token t) {
   errno = 0;
-  long long value = strtoull(s, NULL, 10);
+  long long value = strtod(t.position_in_source, NULL);
   if (errno != 0) {
-    ERROR_AND_EXIT("StringToLL() error");
+    ERROR_AND_EXIT("TokenToDouble() error");
   }
 
   return value;
@@ -82,11 +84,20 @@ bool TypeIsConvertible(AST_Node *from, AST_Node *target_type) {
     return true;
   }
 
+  if (from->type == IDENTIFIER_NODE) {
+    // If the From node is an Identifier, its token
+    // will be the identifier name (not a literal value)
+    // so the TokenTo*() functions won't work properly.
+    //
+    // Ergo, range validation is not performed on identifiers
+    // if their type is not obviously convertible
+    return false;
+  }
+
   if (IsSigned(from) && !IsSigned(target_type)) {
-    long long from_value = StringToLL(from->token.position_in_source);
+    long long from_value = TokenToLL(from->token);
 
     if (from_value < 0) return false;
-
     switch(BitWidth(target_type)) {
       case  8: return from_value < UINT8_MAX;
       case 16: return from_value < UINT16_MAX;
@@ -99,7 +110,7 @@ bool TypeIsConvertible(AST_Node *from, AST_Node *target_type) {
   }
 
   if (!IsSigned(from) && IsSigned(target_type)) {
-    unsigned long long from_value = StringToLL(from->token.position_in_source);
+    unsigned long long from_value = TokenToULL(from->token);
 
     switch(BitWidth(target_type)) {
       case  8: return from_value < INT8_MAX;
@@ -114,7 +125,6 @@ bool TypeIsConvertible(AST_Node *from, AST_Node *target_type) {
 
   return false;
 }
-
 /* === END HELPERS === */
 /* === RANGE VALIDATION === */
 ParserAnnotation ShrinkToSmallestContainingType(AST_Node *node) {
@@ -123,7 +133,7 @@ ParserAnnotation ShrinkToSmallestContainingType(AST_Node *node) {
 
   if (NodeOstensibleType(node) == OST_INT) {
     if (IsSigned(node)) {
-      long long value = StringToLL(node->token.position_in_source);
+      long long value = TokenToLL(node->token);
 
       if (value >= INT8_MIN  && value <= INT8_MAX)  return Annotation(OST_INT,  8, SIGNED);
       if (value >= INT16_MIN && value <= INT16_MAX) return Annotation(OST_INT, 16, SIGNED);
@@ -131,7 +141,7 @@ ParserAnnotation ShrinkToSmallestContainingType(AST_Node *node) {
       if (value >= INT64_MIN && value <= INT64_MAX) return Annotation(OST_INT, 64, SIGNED);
 
     } else {
-      unsigned long long value = StringToULL(node->token.position_in_source);
+      unsigned long long value = TokenToULL(node->token);
 
       if (value <= UINT8_MAX)  return Annotation(OST_INT,  8, UNSIGNED);
       if (value <= UINT16_MAX) return Annotation(OST_INT, 16, UNSIGNED);
@@ -141,7 +151,7 @@ ParserAnnotation ShrinkToSmallestContainingType(AST_Node *node) {
   }
 
   if (NodeOstensibleType(node) == OST_FLOAT) {
-    double d = StringToDouble(node->token.position_in_source);
+    double d = TokenToDouble(node->token);
 
     if (d >= FLT_MIN && d <= FLT_MAX) return Annotation(OST_FLOAT, 32, SIGNED);
     if (d >= DBL_MIN && d <= DBL_MAX) return Annotation(OST_FLOAT, 64, SIGNED);
@@ -153,7 +163,7 @@ ParserAnnotation ShrinkToSmallestContainingType(AST_Node *node) {
 
 bool ValidateIntLiteral(AST_Node *node) {
   if (IsSigned(node)) {
-    long long value = StringToLL(node->token.position_in_source);
+    long long value = TokenToLL(node->token);
 
     switch (BitWidth(node)) {
       case  8: return (value >= INT8_MIN  && value <= INT8_MAX);
@@ -166,7 +176,7 @@ bool ValidateIntLiteral(AST_Node *node) {
             BitWidth(node));
     }
   } else {
-    unsigned long long value = StringToULL(node->token.position_in_source);
+    unsigned long long value = TokenToULL(node->token);
 
     switch (BitWidth(node)) {
       case  8: return (value <= UINT8_MAX);
@@ -184,7 +194,7 @@ bool ValidateIntLiteral(AST_Node *node) {
 }
 
 bool ValidateFloatLiteral(AST_Node *node) {
-  double d = StringToDouble(node->token.position_in_source);
+  double d = TokenToDouble(node->token);
 
   switch (BitWidth(node)) {
     case 32: return (d >= FLT_MIN && d <= FLT_MAX);
@@ -200,7 +210,7 @@ bool ValidateFloatLiteral(AST_Node *node) {
 }
 /* === END RANGE VALIDATION === */
 
-static void Identifier(AST_Node *identifier) {
+static void Assignment(AST_Node *identifier) {
   AST_Node *value = identifier->nodes[LEFT];
   bool types_match = ((ActualType)NodeOstensibleType(identifier) ==
                       NodeActualType(value));
@@ -225,6 +235,11 @@ static void Identifier(AST_Node *identifier) {
     identifier->token.position_in_source,
     AnnotationTranslation(identifier->annotation),
     AnnotationTranslation(identifier->nodes[LEFT]->annotation));
+}
+
+static void Identifier(AST_Node *identifier) {
+  Symbol symbol = RetrieveFrom(SYMBOL_TABLE, identifier->token);
+  identifier->annotation.actual_type = (ActualType)symbol.annotation.ostensible_type;
 }
 
 static void Literal(AST_Node *node) {
@@ -295,10 +310,10 @@ static void BinaryOp(AST_Node *node) {
     (ActualType)node->annotation.ostensible_type;
 }
 
-void CheckTypes(AST_Node *node) {
-  if (node->nodes[LEFT]   != NULL) CheckTypes(node->nodes[LEFT]);
-  if (node->nodes[MIDDLE] != NULL) CheckTypes(node->nodes[MIDDLE]);
-  if (node->nodes[RIGHT]  != NULL) CheckTypes(node->nodes[RIGHT]);
+void CheckTypesRecurse(AST_Node *node) {
+  if (node->nodes[LEFT]   != NULL) CheckTypesRecurse(node->nodes[LEFT]);
+  if (node->nodes[MIDDLE] != NULL) CheckTypesRecurse(node->nodes[MIDDLE]);
+  if (node->nodes[RIGHT]  != NULL) CheckTypesRecurse(node->nodes[RIGHT]);
 
   switch(node->type) {
     case LITERAL_NODE: {
@@ -313,9 +328,20 @@ void CheckTypes(AST_Node *node) {
     case IDENTIFIER_NODE: {
       Identifier(node);
     } break;
+    case DECLARATION_NODE: {
+      // Do nothing
+    } break;
+    case ASSIGNMENT_NODE: {
+      Assignment(node);
+    } break;
     default: {
     } break;
   }
 
-  PrintNode(node);
+  if (node->type != CHAIN_NODE) PrintNode(node);
+}
+
+void CheckTypes(AST_Node *node, SymbolTable *symbol_table) {
+  SYMBOL_TABLE = symbol_table;
+  CheckTypesRecurse(node);
 }
