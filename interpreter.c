626960bacd8a0780790cc60fb049e7f65c2b7bca
rtmath <ceo@ligma.org>
2024-11-10 14:43:07 -0800

    add remaining basic math operations to Interpreter

--- interpreter.c
+++ interpreter.c
@@ -35,7 +35,7 @@ void Assignment(AST_Node *n) {
   } else {
     symbol = NewSymbol(n->token, n->annotation, DECL_NONE);
   }
-  symbol.value  = LEFT_NODE(n)->value;
+  symbol.value = LEFT_NODE(n)->value;
 
   Symbol stored_symbol = AddTo(SYMBOL_TABLE(), symbol);
   PrintSymbol(stored_symbol);
@@ -43,18 +43,178 @@ void Assignment(AST_Node *n) {
   n->value = symbol.value;
 }
 
+Value AddValues(Value v1, Value v2) {
+  if (v1.type != v2.type) ERROR_AND_EXIT("AddValues(): Type mismatch");
+
+  switch(v1.type) {
+    case V_INT: {
+      return (Value){
+        .type = V_INT,
+        .array_type = V_NONE,
+        .as.integer = v1.as.integer + v2.as.integer
+      };
+    } break;
+    case V_UINT: {
+      return (Value){
+        .type = V_INT,
+        .array_type = V_NONE,
+        .as.uinteger = v1.as.uinteger + v2.as.uinteger
+      };
+    } break;
+    case V_FLOAT: {
+      return (Value){
+        .type = V_INT,
+        .array_type = V_NONE,
+        .as.floating = v1.as.floating + v2.as.floating
+      };
+    } break;
+    default: ERROR_AND_EXIT_FMTMSG("AddValues(): Invalid type %d", v1.type);
+  }
+
+  return (Value){ .type = 0, .array_type = 0, .as.integer = 0 };
+}
+
+Value SubValues(Value v1, Value v2) {
+  if (v1.type != v2.type) ERROR_AND_EXIT("SubValues(): Type mismatch");
+
+  switch(v1.type) {
+    case V_INT: {
+      return (Value){
+        .type = V_INT,
+        .array_type = V_NONE,
+        .as.integer = v1.as.integer - v2.as.integer
+      };
+    } break;
+    case V_UINT: {
+      return (Value){
+        .type = V_INT,
+        .array_type = V_NONE,
+        .as.uinteger = v1.as.uinteger - v2.as.uinteger
+      };
+    } break;
+    case V_FLOAT: {
+      return (Value){
+        .type = V_INT,
+        .array_type = V_NONE,
+        .as.floating = v1.as.floating - v2.as.floating
+      };
+    } break;
+    default: ERROR_AND_EXIT_FMTMSG("SubValues(): Invalid type %d", v1.type);
+  }
+
+  return (Value){ .type = 0, .array_type = 0, .as.integer = 0 };
+}
+
+Value MulValues(Value v1, Value v2) {
+  if (v1.type != v2.type) ERROR_AND_EXIT("MulValues(): Type mismatch");
+
+  switch(v1.type) {
+    case V_INT: {
+      return (Value){
+        .type = V_INT,
+        .array_type = V_NONE,
+        .as.integer = v1.as.integer * v2.as.integer
+      };
+    } break;
+    case V_UINT: {
+      return (Value){
+        .type = V_INT,
+        .array_type = V_NONE,
+        .as.uinteger = v1.as.uinteger * v2.as.uinteger
+      };
+    } break;
+    case V_FLOAT: {
+      return (Value){
+        .type = V_INT,
+        .array_type = V_NONE,
+        .as.floating = v1.as.floating * v2.as.floating
+      };
+    } break;
+    default: ERROR_AND_EXIT_FMTMSG("MulValues(): Invalid type %d", v1.type);
+  }
+
+  return (Value){ .type = 0, .array_type = 0, .as.integer = 0 };
+}
+
+Value DivValues(Value v1, Value v2) {
+  if (v1.type != v2.type) ERROR_AND_EXIT("DivValues(): Type mismatch");
+
+  switch(v1.type) {
+    case V_INT: {
+      return (Value){
+        .type = V_INT,
+        .array_type = V_NONE,
+        .as.integer = v1.as.integer / v2.as.integer
+      };
+    } break;
+    case V_UINT: {
+      return (Value){
+        .type = V_INT,
+        .array_type = V_NONE,
+        .as.uinteger = v1.as.uinteger / v2.as.uinteger
+      };
+    } break;
+    case V_FLOAT: {
+      return (Value){
+        .type = V_INT,
+        .array_type = V_NONE,
+        .as.floating = v1.as.floating / v2.as.floating
+      };
+    } break;
+    default: ERROR_AND_EXIT_FMTMSG("DivValues(): Invalid type %d", v1.type);
+  }
+
+  return (Value){ .type = 0, .array_type = 0, .as.integer = 0 };
+}
+
+Value ModValues(Value v1, Value v2) {
+  if (v1.type != v2.type) ERROR_AND_EXIT("ModValues(): Type mismatch");
+
+  switch(v1.type) {
+    case V_INT: {
+      return (Value){
+        .type = V_INT,
+        .array_type = V_NONE,
+        .as.integer = v1.as.integer % v2.as.integer
+      };
+    } break;
+    case V_UINT: {
+      return (Value){
+        .type = V_INT,
+        .array_type = V_NONE,
+        .as.uinteger = v1.as.uinteger % v2.as.uinteger
+      };
+    } break;
+    default: ERROR_AND_EXIT_FMTMSG("ModValues(): Invalid type %d", v1.type);
+  }
+
+  return (Value){ .type = 0, .array_type = 0, .as.integer = 0 };
+}
+
 void Binary(AST_Node *n) {
   switch(n->token.type) {
     case PLUS: {
-      n->value = (Value){
-        .type = V_INT,
-        .array_type = V_NONE,
-        .as.integer = LEFT_NODE(n)->value.as.integer +
-                      RIGHT_NODE(n)->value.as.integer
-      };
+      n->value = AddValues(LEFT_NODE(n)->value,
+                           RIGHT_NODE(n)->value);
+    } break;
+    case MINUS: {
+      n->value = SubValues(LEFT_NODE(n)->value,
+                           RIGHT_NODE(n)->value);
+    } break;
+    case ASTERISK: {
+      n->value = MulValues(LEFT_NODE(n)->value,
+                           RIGHT_NODE(n)->value);
+    } break;
+    case DIVIDE: {
+      n->value = DivValues(LEFT_NODE(n)->value,
+                           RIGHT_NODE(n)->value);
+    } break;
+    case MODULO: {
+      n->value = ModValues(LEFT_NODE(n)->value,
+                           RIGHT_NODE(n)->value);
     } break;
     default: {
-      printf("Not implemented yet\n");
+      printf("Assignment(): Not implemented yet\n");
     } break;
   }
 }
