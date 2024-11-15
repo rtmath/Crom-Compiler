a516d6790c06a51397903a2d8a8c7738a795e871
rtmath <ceo@ligma.org>
2024-11-14 22:07:16 -0800

    adjust includes to reference /src

--- tests/common.h
+++ tests/common.h
@@ -1,11 +1,11 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#include "../compiler.h"
-#include "../interpreter.h"
-#include "../parser.h"
-#include "../token.h"
-#include "../type_checker.h"
-#include "../value.h"
+#include "../src/compiler.h"
+#include "../src/interpreter.h"
+#include "../src/parser.h"
+#include "../src/token.h"
+#include "../src/type_checker.h"
+#include "../src/value.h"
 
 #endif
