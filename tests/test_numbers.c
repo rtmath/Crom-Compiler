1289e42a78d5aeee9273aa1fead4a80e4191a8b8
rtmath <ceo@ligma.org>
2024-11-19 19:20:36 -0800

    add new tests, rename existing tests

--- tests/test_numbers.c
+++ tests/test_numbers.c
@@ -4,75 +4,122 @@
 #include "test_numbers.h"
 
 /* ==== Signed ==== */
-static void Test_I8MaxLimits() {
+static void Test_I8Max_Limit_OK() {
+  COMPILE("i8 check = 127;")
+
+  AssertNoError();
+}
+
+static void Test_I8Max_Overflow() {
   COMPILE("i8 check = 128;") // INT8_MAX + 1
 
   AssertExpectError(ERR_TYPE_DISAGREEMENT);
 }
 
-static void Test_I16MaxLimits() {
+static void Test_I16Max_Limit_OK() {
+  COMPILE("i16 check = 32767;")
+
+  AssertNoError();
+}
+
+static void Test_I16Max_Overflow() {
   COMPILE("i16 check = 32768;") // INT16_MAX + 1
 
   AssertExpectError(ERR_TYPE_DISAGREEMENT);
 }
 
-static void Test_I32MaxLimits() {
+static void Test_I32Max_Limit_OK() {
+  COMPILE("i32 check = 2147483647;")
+
+  AssertNoError();
+}
+
+static void Test_I32Max_Overflow() {
   COMPILE("i32 check = 2147483648;") // INT32_MAX + 1
 
   AssertExpectError(ERR_TYPE_DISAGREEMENT);
 }
 
-static void Test_I64MaxLimits() {
+static void Test_I64Max_Limit_OK() {
+  COMPILE("i64 check = 9223372036854775807;")
+
+  AssertNoError();
+}
+
+static void Test_I64Max_Overflow() {
   COMPILE("i64 check = 9223372036854775808;") // INT64_MAX + 1
 
   AssertExpectError(ERR_OVERFLOW);
 }
 
-static void Test_I8MinLimits() {
+static void Test_I8Min_Limit_OK() {
+  COMPILE("i8 check = -128;")
+
+  AssertNoError();
+}
+
+static void Test_I8Min_Overflow() {
   COMPILE("i8 check = -129;") // INT8_MIN - 1
 
   AssertExpectError(ERR_TYPE_DISAGREEMENT);
 }
 
-static void Test_I16MinLimits() {
+static void Test_I16Min_Limit_OK() {
+  COMPILE("i16 check = -32768;")
+
+  AssertNoError();
+}
+
+static void Test_I16Min_Overflow() {
   COMPILE("i16 check = -32769;") // INT16_MIN - 1
 
   AssertExpectError(ERR_TYPE_DISAGREEMENT);
 }
 
-static void Test_I32MinLimits() {
+static void Test_I32Min_Limit_OK() {
+  COMPILE("i32 check = -2147483648;")
+
+  AssertNoError();
+}
+
+static void Test_I32Min_Overflow() {
   COMPILE("i32 check = -2147483649;") // INT16_MIN - 1
 
   AssertExpectError(ERR_TYPE_DISAGREEMENT);
 }
 
-static void Test_I64MinLimits() {
+static void Test_I64Min_Limit_OK() {
+  COMPILE("i64 check = -9223372036854775808;")
+
+  AssertNoError();
+}
+
+static void Test_I64Min_Overflow() {
   COMPILE("i64 check = -9223372036854775809;") // INT16_MIN - 1
 
   AssertExpectError(ERR_OVERFLOW);
 }
 
-
 /* === Unsigned === */
-static void Test_U8Limits() {
+static void Test_U8_Overflow() {
   COMPILE("u8 check = 256;") // UINT8_MAX + 1
 
   AssertExpectError(ERR_TYPE_DISAGREEMENT);
 }
 
-static void Test_U16Limits() {
+static void Test_U16_Overflow() {
   COMPILE("u16 check = 65536;") // UINT16_MAX + 1
 
   AssertExpectError(ERR_TYPE_DISAGREEMENT);
 }
 
-static void Test_U32Limits() {
+static void Test_U32_Overflow() {
   COMPILE("u32 check = 4294967296;") // UINT32_MAX + 1
 
   AssertExpectError(ERR_TYPE_DISAGREEMENT);
 }
 
-static void Test_U64Limits() {
+static void Test_U64_Overflow() {
   COMPILE("u64 check = 18446744073709551616;") // UINT64_MAX + 1
 
   AssertExpectError(ERR_OVERFLOW);
@@ -85,50 +132,50 @@ static void Test_CannotStoreNegativeUint() {
 }
 
 /* ====  Hex   ==== */
-static void Test_U8HexLimits() {
+static void Test_Hex_U8_Overflow() {
   COMPILE("u8 check = 0x1FF;")
 
   AssertExpectError(ERR_TYPE_DISAGREEMENT);
 }
 
-static void Test_U16HexLimits() {
+static void Test_Hex_U16_Overflow() {
   COMPILE("u16 check = 0x1FFFF;")
 
   AssertExpectError(ERR_TYPE_DISAGREEMENT);
 }
 
-static void Test_U32HexLimits() {
+static void Test_Hex_U32_Overflow() {
   COMPILE("u32 check = 0x1FFFFFFFF;")
 
   AssertExpectError(ERR_TYPE_DISAGREEMENT);
 }
 
-static void Test_U64HexLimits() {
+static void Test_Hex_U64_Overflow() {
   COMPILE("u64 check = 0x1FFFFFFFFFFFFFFFF;")
 
   AssertExpectError(ERR_LEXER_ERROR);
 }
 
 /* ==== Binary ==== */
-static void Test_U8BinaryLimits() {
+static void Test_Binary_U8_Overflow() {
   COMPILE("u8 check = b'111111110';")
 
   AssertExpectError(ERR_TYPE_DISAGREEMENT);
 }
 
-static void Test_U16BinaryLimits() {
+static void Test_Binary_U16_Overflow() {
   COMPILE("u16 check = b'11111111000000001';")
 
   AssertExpectError(ERR_TYPE_DISAGREEMENT);
 }
 
-static void Test_U32BinaryLimits() {
+static void Test_Binary_U32_Overflow() {
   COMPILE("u32 check = b'111111110000000011111111000000001';")
 
   AssertExpectError(ERR_TYPE_DISAGREEMENT);
 }
 
-static void Test_U64BinaryLimits() {
+static void Test_Binary_U64_Overflow() {
   COMPILE("u64 check = b'11111111000000001111111100000000111111110000000011111111000000001';")
 
   // The Lexer creates an Error token when the binary literal exceeds a
@@ -171,28 +218,28 @@ static void Test_UnexpectedTrailingDecimalFloatLiteral() {
   AssertExpectError(ERR_UNEXPECTED);
 }
 
-static void Test_CanStorePositiveFltMax() {
+static void Test_F32MAX_Limit_OK() {
   COMPILE("f32 check = 340282346638528859811704183484516925440.000000;")
 
   AssertNoError();
   AssertEqual(NewFloatValue(FLT_MAX));
 }
 
-static void Test_CanStoreNegativeFltMax() {
+static void Test_F32MIN_Limit_OK() {
   COMPILE("f32 check = -340282346638528859811704183484516925440.000000;")
 
   AssertNoError();
   AssertEqual(NewFloatValue(-FLT_MAX));
 }
 
-static void Test_CanStorePositiveDblMax() {
+static void Test_F64MAX_Limit_OK() {
   COMPILE("f64 check = 179769313486231570814527423731704356798070567525844996598917476803157260780028538760589558632766878171540458953514382464234321326889464182768467546703537516986049910576551282076245490090389328944075868508455133942304583236903222948165808559332123348274797826204144723168738177180919299881250404026184124858368.000000;")
 
   AssertNoError();
   AssertEqual(NewFloatValue(DBL_MAX));
 }
 
-static void Test_CanStoreNegativeDblMax() {
+static void Test_F64MIN_Limit_OK() {
   COMPILE("f64 check = -179769313486231570814527423731704356798070567525844996598917476803157260780028538760589558632766878171540458953514382464234321326889464182768467546703537516986049910576551282076245490090389328944075868508455133942304583236903222948165808559332123348274797826204144723168738177180919299881250404026184124858368.000000;")
 
   AssertNoError();
@@ -207,34 +254,44 @@ static void Test_CannotAssignBigLiteralToF32() {
 
 void RunAllNumberTests() {
   /* ---- Signed ---- */
-  Test_I8MaxLimits();
-  Test_I16MaxLimits();
-  Test_I32MaxLimits();
-  Test_I64MaxLimits();
+  Test_I8Min_Limit_OK();
+  Test_I16Min_Limit_OK();
+  Test_I32Min_Limit_OK();
+  Test_I64Min_Limit_OK();
 
-  Test_I8MinLimits();
-  Test_I16MinLimits();
-  Test_I32MinLimits();
-  Test_I64MinLimits();
+  Test_I8Max_Limit_OK();
+  Test_I16Max_Limit_OK();
+  Test_I32Max_Limit_OK();
+  Test_I64Max_Limit_OK();
+
+  Test_I8Max_Overflow();
+  Test_I16Max_Overflow();
+  Test_I32Max_Overflow();
+  Test_I64Max_Overflow();
+
+  Test_I8Min_Overflow();
+  Test_I16Min_Overflow();
+  Test_I32Min_Overflow();
+  Test_I64Min_Overflow();
 
   /* --- Unsigned --- */
-  Test_U8Limits();
-  Test_U16Limits();
-  Test_U32Limits();
-  Test_U64Limits();
+  Test_U8_Overflow();
+  Test_U16_Overflow();
+  Test_U32_Overflow();
+  Test_U64_Overflow();
   Test_CannotStoreNegativeUint();
 
   /* ----  Hex   ---- */
-  Test_U8HexLimits();
-  Test_U16HexLimits();
-  Test_U32HexLimits();
-  Test_U64HexLimits();
+  Test_Hex_U8_Overflow();
+  Test_Hex_U16_Overflow();
+  Test_Hex_U32_Overflow();
+  Test_Hex_U64_Overflow();
 
   /* ---- Binary ---- */
-  Test_U8BinaryLimits();
-  Test_U16BinaryLimits();
-  Test_U32BinaryLimits();
-  Test_U64BinaryLimits();
+  Test_Binary_U8_Overflow();
+  Test_Binary_U16_Overflow();
+  Test_Binary_U32_Overflow();
+  Test_Binary_U64_Overflow();
   Test_LargeZeroBinaryLiteralInSmallType();
 
   /* ---- Floats ---- */
@@ -242,10 +299,10 @@ void RunAllNumberTests() {
   Test_NegativeFloatLiteral();
   Test_UnexpectedLeadingDecimalFloatLiteral();
   Test_UnexpectedTrailingDecimalFloatLiteral();
-  Test_CanStorePositiveFltMax();
-  Test_CanStoreNegativeFltMax();
-  Test_CanStorePositiveDblMax();
-  Test_CanStoreNegativeDblMax();
+  Test_F32MAX_Limit_OK();
+  Test_F32MIN_Limit_OK();
+  Test_F64MAX_Limit_OK();
+  Test_F64MIN_Limit_OK();
   Test_CannotAssignBigLiteralToF32();
 
   PrintAssertionResults("Numbers");
